d213d709fa27600a288a9482772ac669865dca94
Вадим <54228342+VadimFalaleev@users.noreply.github.com>
2022-12-18 01:13:04 +0500

    EnergyShield

--- Game Service Dev LAB6/Assets/_Scripts/EnergyShield.cs
+++ Game Service Dev LAB6/Assets/_Scripts/EnergyShield.cs
@@ -1,52 +1,26 @@
 using UnityEngine;
 using TMPro;
-using System.Collections;
 
 public class EnergyShield : MonoBehaviour
 {
     public TextMeshProUGUI scoreGT;
     public AudioSource audioSource;
 
-    private RectTransform rectTransform; // new
+    public float _velocity = 10;
 
     private void Start()
     {
         GameObject scoreGO = GameObject.Find("Score");
         scoreGT = scoreGO.GetComponent<TextMeshProUGUI>();
         scoreGT.text = "0";
-
-        rectTransform = GetComponent<RectTransform>(); // new
     }
 
     void Update()
     {
-        //Vector3 mousePos2D = Input.mousePosition;
-        //mousePos2D.z = -Camera.main.transform.position.z;
-
-
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 pos = this.transform.position;
+        Vector2 mousePos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 pos = transform.position;
         pos.x = mousePos3D.x;
-        //this.transform.position = pos;
-
-        if(this.transform.position != pos)
-        {
-            StartCoroutine(DoMove(3f, Vector2.zero));
-        }
-    }
-
-    private IEnumerator DoMove(float time, Vector2 targetPosition)
-    {
-        Vector2 startPosition = rectTransform.anchoredPosition; 
-        float startTime = Time.realtimeSinceStartup; 
-        float fraction = 0f;
-
-        while (fraction < 1f) 
-        { 
-            fraction = Mathf.Clamp01((Time.realtimeSinceStartup - startTime) / time); 
-            rectTransform.anchoredPosition = Vector2.Lerp(startPosition, targetPosition, fraction); 
-            yield return null; 
-        }
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * _velocity);
     }
 
     private void OnCollisionEnter(Collision collision)
